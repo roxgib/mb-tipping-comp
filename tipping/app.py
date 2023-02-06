5d782acf34fc7b99743573a907d752c7b3ecfc85
roxgib <33942237+roxgib@users.noreply.github.com>
2023-02-06 12:49:38 +1100

    Fix static files url

--- tipping/app.py
+++ tipping/app.py
@@ -9,12 +9,13 @@ import flask_login
 with open("../secrets.json") as f:
     _db: dict[str, str] = json.load(f)
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "/tippingcomp/static")
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"postgresql://{_db['username']}:{_db['password']}@{_db['server']}:5432/{_db['database']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "BAD_SECRET_KEY"
+
 db = SQLAlchemy(app)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
